a72d74ff34304a0892f58c7a17c08d3152f44c39
Jason Ellis <je7979@gmail.com>
2015-01-22 15:44:13 -0600

    create route to index.html

--- tickettweets/views.py
+++ tickettweets/views.py
@@ -1,10 +1,9 @@
 from tickettweets import tickettweets
-
+from flask import render_template
 
 @tickettweets.route("/")
-@tickettweets.route("/index")
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     tickettweets.run()
