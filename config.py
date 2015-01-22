52c9b87dd66bb0c93f8b37f9a025361727901f8f
Jason Ellis <je7979@gmail.com>
2015-01-22 15:42:42 -0600

    add initial db info

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WTF_CSRF_ENABLED = True
+# TODO create a secret key
+SECRET_KEY = 'you-will-never-guess'
--- tickettweets/__init__.py
+++ tickettweets/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 tickettweets = Flask(__name__)
+db = SQLAlchemy(tickettweets)
 
 # import after tickettweets to prevent circular import
 from tickettweets import views
