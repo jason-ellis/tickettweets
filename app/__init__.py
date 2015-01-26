969d37f266e0f01c74ea9241ec1ffdcf8e3351a7
Jason Ellis <je7979@gmail.com>
2015-01-26 12:46:20 -0600

    add mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-
-from app import stream
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 
+# Start MongoDB
+app.config['MONGO_DBNAME'] = 'tickettweets'
+mongo = PyMongo(app, config_prefix='MONGO')
 
 # Start the Twitter stream
+from app import stream
 stream.main()
 
 # import after tickettweets to prevent circular import
