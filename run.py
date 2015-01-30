015af1e5c6b6d0e58bae4836a7505ae83ca3b968
Jason Ellis <je7979@gmail.com>
2015-01-29 20:10:16 -0600

    revert to simply script, call app.run as thread

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 #!venv/bin/python
 from app import app
-from multiprocessing import Process
-
-p2 = Process(target=app.run(debug=True))
-p2.daemon = True
-p2.start()
+app.run(debug=True, threaded=True)
