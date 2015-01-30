420ee22a8480b4fcfa70b4b6297990f908cc4dfe
Jason Ellis <je7979@gmail.com>
2015-01-29 20:05:32 -0600

    add flask-bootstrap, move stream start back to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from pymongo import Connection
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 # Start MongoDB
 conn = Connection()
 db = conn.tickettweets
 collection = db.tweets
 
+from stream import start_stream
+start_stream()
+
 # import after app to prevent circular import
 from app import views
 
-# Start the Twitter stream
-from app import stream
-stream.main()
