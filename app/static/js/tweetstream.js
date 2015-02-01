2345f8ba8367996dc89166a1086f6c07e7b79180
Jason Ellis <je7979@gmail.com>
2015-02-01 14:05:24 -0600

    move to server-side rendering

--- app/static/js/tweetstream.js
+++ app/static/js/tweetstream.js
@@ -15,10 +15,8 @@ $(document).ready(function() {
                 {cursor: getCursor()},
                 function(new_tweets) {
                     if(debug) {
-                        //logNewTweets(new_tweets);
-                        console.log(new_tweets);
+                        console.log('New tweets: ' + new_tweets);
                     }
-                    //updateStream(new_tweets, 'new');
                     $('#tweet_container').prepend(new_tweets);
                 });
             return false;
@@ -29,23 +27,17 @@ $(document).ready(function() {
     // Submits cursor to _more_tweets and receives older tweet objects
     $(function () {
         $('a#more_tweets').bind('click', function () {
-            $.getJSON($SCRIPT_ROOT + '/_more_tweets',
+            $.get($SCRIPT_ROOT + '/_more_tweets',
                 {last_tweet_id: getOldest()},
                 function(old_tweets) {
                     if(debug) {
-                        logNewTweets(old_tweets);
+                        console.log('Added tweets: ' + old_tweets);
                     }
-                    updateStream(old_tweets, 'old');
+                    $('#tweet_container').append(old_tweets);
                 });
             return false;
         });
     });
-
-    function logNewTweets(tweets) {
-        console.log(tweets.length + ' tweets returned');
-                    console.log('new_tweets: ');
-                    console.log(tweets);
-    }
 
     // Fetches data-tweet of each tweet and returns the highest value as cursor
     function getCursor() {
@@ -83,56 +75,4 @@ $(document).ready(function() {
 
     getCursor();
 
-    // Add new tweets to DOM
-    function updateStream(newTweets, age) {
-        var sortedTweets = newTweets.sort(function(a,b) {
-            return parseInt(a['_id']) - parseInt(b['_id'])
-        });
-        var addedTweets = [];
-        $.each(sortedTweets, function(k, v) {
-            addedTweets.push(v['_id']);
-            if(age=='new') {
-                $('#tweet_container').prepend(formatTweet(v));
-            } else if(age=='old') {
-                $('#tweet_container').append(formatTweet(v));
-            }
-        });
-        if(debug) {
-            console.log('Added these new tweets: ' + addedTweets);
-        }
-    }
-
-    // Create link to profile
-    function linkProfile(id) {
-        return "https://twitter.com/intent/follow?user_id=" + id + "/"
-    }
-
-    // Create link to status
-    function linkStatus(id, statusId) {
-        return "http://twitter.com/" + id + "/status/" + statusId + "/"
-    }
-
-    // format individual tweet for output to the DOM
-    function formatTweet(tweet) {
-        var tweetId = tweet['id_str'];
-        var userId = tweet['user']['id_str'];
-        var profileImageUrl = tweet['user']['profile_image_url'];
-        var userName = tweet['user']['name'];
-        var screenName = tweet['user']['screen_name'];
-        var tweetText = tweet['text'];
-
-        return '<div class="tweet" data-tweet="' + tweetId + '">' +
-            '<a href="' + linkProfile(userId) + '" target="_blank">' +
-            '<img class="profile_img" src="' + profileImageUrl + '" alt="profile image">' +
-            '<span class="user_name">' + userName + '</span><br>' +
-            '<span class="screen_name">@' + screenName + '</span></a><br>' +
-            '<div class="tweet_text">' + tweetText + '</div>' +
-                '<a href="' + linkStatus(screenName,tweetId) + '" target="_blank">Link to tweet</a>' +
-            '</div>';
-    }
-
-    // Process media entities for a tweet
-    function formatTweetText(tweet) {
-        var tweetText = tweet['text']
-    }
 });
--- app/views.py
+++ app/views.py
@@ -18,7 +18,7 @@ def index():
                            tweets=tweets,
                            debug=debug)
 
-
+# AJAX call for new tweets
 @app.route("/_new_tweets")
 def new_tweets():
     cursor_id = request.args.get('cursor', 0, type=int)
@@ -43,36 +43,8 @@ def new_tweets():
                         tweet=tweet,
                         debug=debug)
         updated_tweets.append(tweet_html)
-    updated_tweets = ' '.join(updated_tweets)
+    updated_tweets = '\n'.join(updated_tweets)
     return updated_tweets
-
-# AJAX call for new tweets
-"""@app.route("/_new_tweets")
-def new_tweets():
-    cursor_id = request.args.get('cursor', 0, type=int)
-    if debug:
-        try:
-            cursor_tweet = collection.find_one({'_id': cursor_id})
-            print('Cursor tweet {0} created at {1}'
-                  .format(cursor_id,
-                          cursor_tweet['created_at']))
-        except TypeError as e:
-            print('**Error encountered finding the cursor. {} not found'
-                  .format(cursor_id))
-            print('**TypeError: {}'.format(e))
-        except:
-            e = sys.exc_info()[0]
-            print('**Other Error: {}'.format(e))
-    updated_tweets = []
-    for tweet in collection.find({'_id': {'$gt': cursor_id}}).sort('_id', -1):
-        print('tweet {0} created at {1}'.format(tweet['_id'],
-                                                tweet['created_at']))
-        updated_tweets.append(tweet)
-    updated_tweets = json.dumps(updated_tweets)
-    resp = Response(response=updated_tweets,
-                    status=200,
-                    mimetype="application/json")
-    return resp"""
 
 # AJAX call for more older tweets
 @app.route("/_more_tweets")
@@ -96,9 +68,9 @@ def more_tweets():
             .sort('_id', -1).limit(20):
         print('tweet {0} created at {1}'.format(tweet['_id'],
                                                 tweet['created_at']))
-        older_tweets.append(tweet)
-    older_tweets = json.dumps(older_tweets)
-    resp = Response(response=older_tweets,
-                    status=200,
-                    mimetype="application/json")
-    return resp
+        tweet_html = render_template('tweet.html',
+                        tweet=tweet,
+                        debug=debug)
+        older_tweets.append(tweet_html)
+    older_tweets = '\n'.join(older_tweets)
+    return older_tweets
