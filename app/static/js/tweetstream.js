a0af87a1b8011599c25ad13397153eb0610d5f4a
Jason Ellis <je7979@gmail.com>
2015-02-05 14:04:27 -0600

    add twitter web intents JS, add my own status open action

--- app/static/js/tweetstream.js
+++ app/static/js/tweetstream.js
@@ -71,4 +71,73 @@ $(document).ready(function() {
         }
         return oldestTweet;
     }
+
+    // Twitter web intent JS from https://dev.twitter.com/web/intents
+    (function() {
+      if (window.__twitterIntentHandler) return;
+      var intentRegex = /twitter\.com(\:\d{2,4})?\/intent\/(\w+)/,
+          windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes',
+          width = 550,
+          height = 420,
+          winHeight = screen.height,
+          winWidth = screen.width;
+
+      // Regex for status links to open statuses in sized window
+      var replyRegex = /twitter\.com?\/(\w+)?\/(status)\/(\w+)/,
+          replyWidth = 700,
+          replyHeight = 500;
+
+      function handleIntent(e) {
+        e = e || window.event;
+        var target = e.target || e.srcElement,
+            m, r, left, top;
+
+        while (target && target.nodeName.toLowerCase() !== 'a') {
+          target = target.parentNode;
+        }
+
+        if (target && target.nodeName.toLowerCase() === 'a' && target.href) {
+          m = target.href.match(intentRegex);
+          r = target.href.match(replyRegex);
+          if (m || r) {
+            if(m) {
+              left = Math.round((winWidth / 2) - (width / 2));
+              top = 0;
+
+              if (winHeight > height) {
+                top = Math.round((winHeight / 2) - (height / 2));
+              }
+
+              window.open(target.href, 'intent', windowOptions + ',width=' + width +
+                ',height=' + height + ',left=' + left + ',top=' + top);
+                console.log(target.href, 'intent', windowOptions + ',width=' + width +
+                ',height=' + height + ',left=' + left + ',top=' + top)
+            } else if(r) {
+              left = Math.round((winWidth / 2) - (replyWidth / 2));
+              top = 0;
+
+              if (winHeight > replyHeight) {
+                top = Math.round((winHeight / 2) - (replyHeight / 2));
+              }
+
+              window.open(target.href, 'reply', windowOptions + ',width=' +
+                replyWidth + ',height=' + replyHeight + ',left=' + left +
+                ',top=' + top);
+                console.log(target.href, 'reply', windowOptions + ',width=' +
+                replyWidth + ',height=' + replyHeight + ',left=' + left +
+                ',top=' + top);
+            }
+            e.returnValue = false;
+            e.preventDefault && e.preventDefault();
+          }
+        }
+      }
+
+      if (document.addEventListener) {
+        document.addEventListener('click', handleIntent, false);
+      } else if (document.attachEvent) {
+        document.attachEvent('onclick', handleIntent);
+      }
+      window.__twitterIntentHandler = true;
+    }());
 });
